6032e180a25cead7e2c3cb1ca95004c8bae0d99d
boyoanu <boyoanu@hotmail.com>
2024-09-13 16:06:11 +0100

    add files

---
+++ BasicMathTDDTest/UnitTest2.cs
@@ -0,0 +1,42 @@
+using BasicMathematics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicMathTDDTest
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.divide(10, 5);
+            Assert.AreEqual(res, 2);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(res, 100);
+        }
+    }
+}
---
+++ BasicMathematics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicMathematics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicMaths basicMaths = new BasicMaths();
+         
+
+           double result1 = basicMaths.Add(48.9, 45.2);
+
+            double result2 = basicMaths.Add(46, 10);
+
+            Console.WriteLine(result1);
+
+            Console.WriteLine(result2);
+        }
+    }
+}
